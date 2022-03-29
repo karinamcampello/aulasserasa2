23b4abff1dfbc3ef90f91ab43d1eafc4de5117c5
karinamcampello <karinamcmenezes@hotmail.com>
2022-03-28 22:21:29 -0300

    Introdução a POO

---
+++ fgabank/Pessoa.java
@@ -0,0 +1,77 @@
+package fgabank;
+
+public class Pessoa {
+
+	
+private	String nome;
+private	String profissao;
+private String cpf;
+private	String rg;
+private	String email;
+private String telefone;
+
+
+public Pessoa() { //método construtor default = preenchimento dos atributos é opcional
+	
+}
+
+public Pessoa(String nome, String cpf, String rg){
+	this.nome = nome;
+	this.cpf = cpf;
+	this.rg = rg;
+	
+	
+	
+}
+
+
+public Pessoa(String nome, String profissao, String cpf, String rg, String email, String telefone) {
+	super();
+	this.nome = nome;
+	this.profissao = profissao;
+	this.cpf = cpf;
+	this.rg = rg;
+	this.email = email;
+	this.telefone = telefone;
+}
+
+public String getNome() { //exibe os objetos
+	return nome;
+}
+public void setNome(String nome) { //grava os objetos no atributo
+	this.nome = nome;
+}
+public String getProfissao() {
+	return profissao;
+}
+public void setProfissao(String profissao) {
+	this.profissao = profissao;
+}
+public String getCpf() {
+	return cpf;
+}
+public void setCpf(String cpf) {
+	this.cpf = cpf;
+}
+public String getRg() {
+	return rg;
+}
+public void setRg(String rg) {
+	this.rg = rg;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getTelefone() {
+	return telefone;
+}
+public void setTelefone(String telefone) {
+	this.telefone = telefone;
+}
+    
+
+	
+}
---
+++ fgabank/TestaBanco.java
@@ -0,0 +1,71 @@
+package fgabank;
+
+public class TestaBanco {
+	
+	public static void main(String[] args) {
+		
+//		Pessoa pessoaNumero1 = new Pessoa(); //criar uma inst�ncia (c�pia)
+//		pessoaNumero1.nome="Jose das Couves";
+//		pessoaNumero1.profissao="Agricultor";
+//		pessoaNumero1.cpf="643.829.641-48";
+//		
+//		System.out.println("Nome: "+pessoaNumero1.nome);
+//		System.out.println("Profiss�o: "+pessoaNumero1.profissao);
+//		System.out.println("CPF: "+pessoaNumero1.cpf);
+//		
+//		Pessoa pessoaNumero2 = pessoaNumero1;
+//		pessoaNumero2.nome = "Jo�o dos Tomates";
+//		
+//		System.out.println("=======================");
+//		
+//		System.out.println("Nome: "+pessoaNumero1.nome);
+//		System.out.println("Nome: "+pessoaNumero2.nome);
+		
+		
+		
+	//	Pessoa pessoaNova = new Pessoa(); //Instanciamos a classe Pessoa
+	//	pessoaNova.setNome("José das Couves");
+	//	System.out.println("Nome: "+ pessoaNova.getNome());
+		
+	//	Pessoa pessoaComConstrutor1 = new Pessoa("João dos Tomates", "259.800.966-55","123,456-78");
+		
+	//	 System.out.println("Nome: " + pessoaComConstrutor1.getNome());
+	//	 System.out.println("RG: " + pessoaComConstrutor1.getRg());
+	//	 System.out.println("CPF: " + pessoaComConstrutor1.getCpf());
+		
+		Conta conta1 = new Conta();
+		conta1.setAgencia(1234);
+		conta1.setNumeroConta(7889);
+		//conta1.setSaldo(1000000.50); não se deve alterar diretamente esta informação. Ela apenas pode ser exibida.
+		conta1.setTipoConta("Flavia Almeida");
+		
+		conta1.depositar(100000);
+		
+		System.out.println("Agencia: "+conta1.getAgencia());
+		System.out.println("Conta: "+conta1.getNumeroConta());
+		System.out.println("Titular: "+conta1.getTitular());
+		System.out.printf("Saldo Atual: R$ %.2f\n ",conta1.getSaldo());
+		
+		if(conta1.sacar(20.0)) {
+			System.out.println("Saque efetuado com sucesso!");
+		}else {
+			System.out.println("Verifique valor do saldo!");
+		}
+		
+		System.out.printf("Saldo Atual: R$ %.2f\n ",conta1.getSaldo());
+		
+		Conta conta2 = new Conta();
+			conta2.setAgencia(5582);
+			conta2.setNumeroConta(2234);
+			
+			conta1.transferir(10000.0, conta2);
+			
+			System.out.printf("Saldo Atual Flavia: R$ %.2f\n ",conta1.getSaldo());
+			System.out.printf("Saldo Atual Babakuxy: R$ %.2f\n ",conta2.getSaldo());
+				
+			
+		}
+		
+	}
+
+
